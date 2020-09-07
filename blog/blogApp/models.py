f0e424727820ef2516c82faa6c2512f460a50f86
Jac00bv <jacobyv11@gmail.com>
2020-09-07 12:00:41 +0200

    add post images

--- blog/blogApp/models.py
+++ blog/blogApp/models.py
@@ -1,11 +1,17 @@
 from django.db import models
-
+import os
 # Create your models here.
+
+
+# def get_image_path(instance, filename):
+#     upload_to = 'blogApp/static/posts/images/posts'
+#     return os.path.join(upload_to, filename)
 
 
 class Post(models.Model):
     post_author = models.CharField(max_length=50)
     post_title = models.CharField(max_length=50)
+    post_image = models.ImageField(upload_to='post_images', null=True)
     post_body = models.CharField(max_length=5000)
     post_date = models.DateTimeField('date published')
 
--- blog/blogApp/views.py
+++ blog/blogApp/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from .models import Post
+import os
 
 
 def index(request):
+    print(os.path.join('static', 'posts', 'images', 'posts'))
     posts_list = Post.objects.order_by('-post_date')
     context = {'posts_list': posts_list}
     return render(request, 'posts/index.html', context)
