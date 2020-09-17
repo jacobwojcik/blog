1f9b1c376c1e7b4a9abe803d5c7d48a6b85f6790
Jac00bv <jacobyv11@gmail.com>
2020-09-17 22:02:37 +0200

    heroku deployment v3

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -10,10 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os.path
+
 from pathlib import Path
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+ROOT_PATH = os.path.dirname(__file__)
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -132,4 +134,4 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = (os.path.join('static'), )
+STATICFILES_DIRS = [os.path.normpath('blogApp/static')]
---
+++ blog/staticfiles/posts/js/script.js
@@ -0,0 +1,16 @@
+const burger = document.querySelector(".burger");
+const navMenu = document.querySelector(".nav-menu");
+burger.addEventListener("click", () => {
+  navMenu.classList.toggle("nav-active");
+  console.log("toggle");
+});
+const titleText = "Welcome to my blog";
+const titleBar = document.querySelector("#title-h1");
+let index = 0;
+const type = () => {
+  titleBar.textContent = titleText.slice(0, index);
+  index++;
+};
+if (titleBar) {
+  setInterval(type, 350);
+}
